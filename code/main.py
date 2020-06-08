3d30241c037d507c65942fff07f69d883ee061b1
Rodrigo Didier <didier.rda@gmail.com>
2020-06-07 21:53:30 -0300

    feature(code) call to execution_w_to_dtime_list, dif_dtime_in_s

--- code/main.py
+++ code/main.py
@@ -1,7 +1,15 @@
 from module1 import read_json
+from module1 import execution_window_to_dtime_list
+
 
 if __name__ == '__main__':
     path_src = '../src'
     label_moc = 'moc.json'
+
+    # read to dict from json
     dir_data = read_json(path_src, label_moc)
-    print(dir_data)
+
+    # get list of dtime objetct (inf and sup boundry)
+    list_dtime_window = []
+    list_dtime_window = execution_window_to_dtime_list(dir_data)
+    #print(execution_window_to_dtime_list(dir_data))
